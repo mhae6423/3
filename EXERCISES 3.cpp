f8a9e0ed1589a1a94e875bd358f5f567cedbc176
mhae6423 <doncillo.trexiemhae@llcc.edu.ph>
2025-12-09 21:17:43 +0800

    Add files via upload

---
+++ EXERCISES 3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Welcome to C++ programming." << endl;
+    cout << "Have a great day!" << endl;
+    return 0;
+}
+
